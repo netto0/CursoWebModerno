e93333fe1f30251ed1bfa036e40fb841431e4051
Netto <orlandiomnetto@gmail.com>
2022-06-01 17:25:23 -0300

    commit exercícios 10, 11

---
+++ exercíciosJavaScript/10.js
@@ -0,0 +1,9 @@
+/*Crie uma função que verifica se um número inteiro passado como parâmetro é divisível por 3 e retorne true
+ou false.*/
+
+function divisivel(num){
+    if (num % 3 == 0){return true}
+    else {return false} 
+}
+
+console.log(divisivel(10))
