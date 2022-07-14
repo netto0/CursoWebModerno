0507522fb1858052389a7281883d3cafdfe603d3
netto <orlandiomnetto@gmail.com>
2022-07-13 21:52:32 -0300

    estudo

--- 7esnext/treinandoCallback.js
+++ 7esnext/treinandoCallback.js
@@ -1,26 +1,17 @@
-const loginUser = (nome, senha, callback) => {
-    setTimeout(() => {
-    console.log('Executando loginUser...')
-    console.log({nome, senha})
-    callback({nome,senha})
-},1500)
+const soma1 = (valor, callback) => {
+    console.log(`${valor} + 1 = ${valor+1}`)
+    callback(valor+1)
 }
 
-const trocaLetras = (nome, callback) => {
-    setTimeout(() => {
-    console.log('Executando trocaLetras...')
-    alterado = nome.replace('e','1')
-    console.log(alterado)
-    callback(alterado)
-}, 1500)
-}
+conta = soma1(1, (resultado1) => {
+    soma1(resultado1, (resultado2) => {
+        soma1(resultado2, (resultado3) => {
+            console.log('Execução finalizada')
+        })
+    })
+})
 
-const primeiraLetra = (nome) => {
-    setTimeout(() => {
-    console.log('Executando primeiraLetra...')
-    console.log(nome[0])
-}, 1500)
-}
+// Para utilizarmos callback em uma função temos que primeiro passá-la como parâmetro e depois utilizá-la como se fosse o 
+// return da função
 
-
-loginUser('Pedro', '1234', (dados) => trocaLetras(dados.nome, (alterado) => primeiraLetra(alterado)))
+// Se tornam inviáveis quando é preciso aninhar muitas callbacks, formando o que é conhecido como "callback hell"
--- 7esnext/treinandoPromises.js
+++ 7esnext/treinandoPromises.js
@@ -1,17 +1,30 @@
-const loginUser = (nome, senha) => {
-        return new Promise((resolve, reject) => {
-            const error = false
-            if (error) {
-                reject('Mensagem de Erro')
-            }
-            console.log('Executando loginUser')
-            resolve({ nome, senha })
-        })
-}
-
-const primeiraLetra = (nome) => {
+const soma1 = (valorInicial) => {
     return new Promise((resolve, reject) => {
-        resolve(nome[0])
+        let error = false
+        somado = valorInicial + 1
+        if (somado == 5){
+            error = true
+        }
+        if (error) {
+            reject('Mensagem de Erro')
+        }
+
+        console.log(somado)
+        resolve(somado)
     })
 }
-loginUser('Carlitos','abcd1234').then((dados) => primeiraLetra(dados))
+
+soma1(0)
+.then(soma1)
+.then(soma1)
+.then(soma1)
+.then(soma1)
+.catch((e) => console.log(e))
+
+// As promises vieram para resolver esse problema gerado pelas callbacks. Para escrever uma promise passamos os parametros da função
+// normalmente, e utilizamos o "return new Promise()" essa promisse recebe como parametro uma callback que recebe por padrão os 
+// parâmetros "resolve" e "reject", na resolução dessa callback o resolve é executado se tudo der certo e o reject é caso dê algum
+// erro ou a função seja "rejeitada"
+
+// Para utilizar uma Promise escrevemos a função e passamos o seus parâmetros normalmente, porém para acessar o resolve utilizamos
+// ".then()" o then receberá o conteúdo do resolve da promise descrita, já para acessar o reject utilizamos o ".catch()"
