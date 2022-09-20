4333e0fee508ee2e3c4f4e5c17515b3e19c767c2
Netto <orlandiomnetto@gmail.com>
2022-09-19 22:05:36 -0300

    nnnnn

--- dom/js/flappy.js
+++ dom/js/flappy.js
@@ -73,7 +73,40 @@ function Barreiras(altura, largura, abertura, espaco, notificarPonto) {
 
 const barreiras = new Barreiras(700,1200,300,400)
 const areaDoJogo = document.querySelector('[wm-flappy')
+const passaro = new Passaro(700)
+areaDoJogo.appendChild(passaro.elemento)
 barreiras.pares.forEach(par => areaDoJogo.appendChild(par.elemento))
 setInterval(() => {
     barreiras.animar()
+    passaro.animar()
 },20)
+
+function Passaro(alturaJogo) {
+    let voando = false
+    this.elemento = novoElemento('img','passaro')
+    this.elemento.src = 'imgs/passaro.png'
+
+    this.getY = () => parseInt(this.elemento.style.bottom.split('px')[0])
+    this.setY = y => this.elemento.style.bottom = `${y}px`
+
+    window.onkeydown = e => voando = true
+    window.onkeyup = e => voando = false
+
+    this.animar = () => {
+        const novoY = this.getY() + (voando ? 8 : -5)
+        const alturaMaxima = alturaJogo - this.elemento.clientHeight
+
+        if (novoY <= 0) {
+            this.setY(0)
+        }
+        else if (novoY >= alturaMaxima) {
+            this.setY(alturaMaxima)
+        } 
+        else {
+            this.setY(novoY)
+        }
+    }
+    this.setY(alturaJogo / 2)
+}
+
+
