ba6a2020020b91b81ef7f17100133b5e57a405c3
Netto <orlandiomnetto@gmail.com>
2023-05-30 18:50:44 -0300

    Most Used Words com erro

---
+++ vue/most-used-words/src/backend/index.js
@@ -0,0 +1,10 @@
+const {ipcMain} = require("electron")
+
+ipcMain.on("process-subtitles", (event, paths) => {
+    console.log(paths)
+    event.reply("process-subtitles", [
+        {word: "i", amount: 547},
+        {word: "you", amount: 478},
+        {word: "it", amount: 10}
+    ])
+})
